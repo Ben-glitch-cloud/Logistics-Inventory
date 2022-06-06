64e9ad412c6bf47a297b4b9d34e80171b4e2f6fb
Ben-glitch-cloud <BTLawrence44@gmail.com>
2022-06-06 12:18:19 +0100

    Viewing inventory list

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const port = 3000
 app.set('view engine', 'ejs');
 app.engine('ejs', require('ejs').__express);
 
-app.get('/viewItems', (req, res) => {
-  res.render('items', {name: 'ben'})
+app.get('/viewItems', (req, res) => { 
+    let ItemsList = [{id: 0, itemName: 'Paper sheets', itemDescription: 'HR White Office A4 Paper 80gsm (Pack of 2500) HR F0317', quantity: 4, wareHouse: false, Date: "04/05/2022"}]
+    res.render('items', {InventoryItemsList: ItemsList})
 })
 
 app.listen(port, () => {
